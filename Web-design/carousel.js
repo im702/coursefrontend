52c6c9003b7c106e79b158cbd6b391f9546d11fe
Марина Муха <annabel9870@gmail.com>
2025-05-21 11:20:32 +0300

    Web desighn

---
+++ Web-design/carousel.js
@@ -0,0 +1,23 @@
+ document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll(".carousel-image");
+    let current = 0;
+
+    function showImage(index) {
+      images.forEach((img, i) => {
+        img.classList.toggle("active", i === index);
+      });
+    }
+
+    document.getElementById("next").addEventListener("click", () => {
+      current = (current + 1) % images.length;
+      showImage(current);
+    });
+
+    document.getElementById("prev").addEventListener("click", () => {
+      current = (current - 1 + images.length) % images.length;
+      showImage(current);
+    });
+
+   
+    showImage(current);
+  })
